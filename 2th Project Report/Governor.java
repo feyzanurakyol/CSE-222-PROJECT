d8f250a1398e26a9ef22c2943e86bcf07c98b5e8
Miray Yıldız <32713493+mirayyildizz@users.noreply.github.com>
2020-04-18 20:27:06 +0300

    Add files via upload

---
+++ 2th Project Report/Governor.java
@@ -0,0 +1,87 @@
+
+
+public class Governor {
+	private String name;
+	private String surname;
+	private String TCNumber;
+	/*private static queue (Visitor class type) */
+	/*private static Binary Search Tree ( Inmade class type) */
+	/*private static LinkedList or ArrayList (Personnel class type) */
+	/*private static LinkedList or ArrayList (Jailer class type) */
+	public Governor(String name, String surname, String TCNumber)
+	{
+		this.name = name;
+		this.surname = surname;
+		this.TCNumber = TCNumber;
+	}
+	
+	public String getName() 
+	{
+		return name;
+	}
+	
+	public String getSurname()
+	{
+		return surname;
+	}
+	
+	public String getTCNumber()
+	{
+		return TCNumber;
+	}
+	
+	public void setName(String name)
+	{
+		this.name = name;
+	}
+	
+	public void setSurname(String surname)
+	{
+		this.surname = surname;
+	}
+	
+	public void setTCNumber(String TCNumber)
+	{
+		this.TCNumber = TCNumber;
+	}
+	
+	public void addJailer(/*Jailer type object OR name, surname, TC, etc.*/)
+	{
+		
+	}
+	
+	public void removeJailer(/*Jailer type object OR jailer informations */)
+	{
+		
+	}
+	
+	public void addInmate(/*Inmate type object OR prisoner informations */)
+	{
+		
+	}
+	
+	public void removeInmate(/* Inmate type object OR prisoner informations */)
+	{
+		
+	}
+	
+	public void addPersonnel(/* Prisoner type object OR prisoner informations */)
+	{
+		
+	}
+	
+	public void removePersonnel(/* Prisoner type object OR prisoner informations */)
+	{
+		
+	}
+	
+	public void addVisitor(/* Visitor type object OR visitor informations*/)
+	{
+		
+	}
+	
+	public void updateVisitorInformation(/* Visitor information */)
+	{
+		/* This method updated visitor's exit time. */
+	}
+}
---
+++ 2th Project Report/Visitor.java
@@ -0,0 +1,109 @@
+
+public class Visitor {
+	String name;
+	String surname;
+	String TCNumber;
+	String inmateNumber;
+	String telephoneNumber;
+	String date;
+	String entrance_time;
+	String exit_time;
+	
+	public Visitor(String name, String surname, String TCNumber, String inmateNumber, String telephoneNumber, String date, String entrance_time)
+	{
+		this.name = name;
+		this.surname = surname;
+		this.TCNumber = TCNumber;
+		this.inmateNumber = inmateNumber;
+		this.telephoneNumber = telephoneNumber;
+		this.date = date;
+		this.entrance_time = entrance_time;
+		this.exit_time = null;
+	}
+	
+	public String getName()
+	{
+		return name;
+	}
+	
+	public String getSurname()
+	{
+		return surname;
+	}
+	
+	public String getTCNumber()
+	{
+		return TCNumber;
+	}
+	
+	public String getInmateNumber()
+	{
+		return inmateNumber;
+	}
+	
+	public String getTelephoneNumber()
+	{
+		return telephoneNumber;
+	}
+	
+	public String getDate()
+	{
+		return date;
+	}
+	
+	public String getEntranceTime()
+	{
+		return entrance_time;
+	}
+	
+	public String getExitTime()
+	{
+		return exit_time;
+	}
+	
+	public void setName(String name)
+	{
+		this.name = name;
+	}
+	
+	public void setSurname(String surname)
+	{
+		this.surname = surname;
+	}
+	
+	public void setTCNumber(String TCNumber)
+	{
+		this.TCNumber = TCNumber;
+	}
+	
+	public void setInmadeName(String inmateNumber)
+	{
+		this.inmateNumber = inmateNumber;
+	}
+	
+	public void setTelephoneNumner(String telephoneNumber)
+	{
+		this.telephoneNumber = telephoneNumber;
+	}
+	
+	public void setDate(String date)
+	{
+		this.date = date;
+	}
+	
+	public void setEntranceTime(String entrance_time)
+	{
+		this.entrance_time = entrance_time;
+	}
+	
+	public void setExitTime(String exit_time)
+	{
+		this.exit_time = exit_time;
+	}
+	
+	
+	
+	
+	
+	
+}
