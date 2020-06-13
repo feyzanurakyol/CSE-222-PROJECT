63a421ec146e6634f79e7950067ab655c634ccf1
Feyza Nur Akyol <24390334+feyzanurakyol@users.noreply.github.com>
2020-06-13 14:58:12 +0300

    menu added

--- Final Report/DataBase and ToDo/src/com/Group1/ChiefJailer.java
+++ Final Report/DataBase and ToDo/src/com/Group1/ChiefJailer.java
@@ -18,7 +18,50 @@ public class ChiefJailer extends Jailer {
     }
 
     @Override
-    public void personnelInterface() {
+        public void personnelInterface() {
+        int k;
+        for ( k = 0; k < 45; k++) System.out.print("-");
+        System.out.print("\n"+"   ");
+        System.out.println("Welcome Chief Jailer " + name);
+        for ( k = 0; k < 45; k++) System.out.print("-");
+        System.out.print("\n"+"   ");
+        System.out.println("What Do you want to do ?");
+        for ( k = 0; k < 45; k++) System.out.print("-");
+        System.out.print("\n"+"   ");
+        System.out.println("[1] Add a visitor");
+        for ( k = 0; k < 45; k++) System.out.print("-");
+        System.out.print("\n"+"   ");
+        System.out.println("[2] Remove a visitor");
+        for ( k = 0; k < 45; k++) System.out.print("-");
+        System.out.print("\n"+"   ");
+        System.out.println("[3] Clear all visitors");
+        for ( k = 0; k < 45; k++) System.out.print("-");
+        System.out.print("\n"+"   ");
+        System.out.println("[4] Get a prisoner");
+        for ( k = 0; k < 45; k++) System.out.print("-");
+        System.out.print("\n"+"   ");
+        System.out.println("[5] Get your shift our");
+        for ( k = 0; k < 45; k++) System.out.print("-");
+        System.out.print("\n"+"   ");
+        System.out.println("[6] Get your department");
+        for ( k = 0; k < 45; k++) System.out.print("-");
+        System.out.print("\n"+"   ");
+        System.out.println("[7] Set jailer's shift our.");
+        for ( k = 0; k < 45; k++) System.out.print("-");
+        System.out.print("\n"+"   ");
+        System.out.println("[8] Manage jailer's department.");
+        for ( k = 0; k < 45; k++) System.out.print("-");
+        System.out.print("\n"+"   ");
+        System.out.println("[9] Set your your shift our.");
+        for ( k = 0; k < 45; k++) System.out.print("-");
+        System.out.print("\n"+"   ");
+        System.out.println("[10] Set your your department");
+        for ( k = 0; k < 45; k++) System.out.print("-");
+        System.out.print("\n"+"   ");
+        System.out.println("[0] Main Menu.");
+        for ( k = 0; k < 45; k++) System.out.print("-");
+        System.out.print("\n");
+        System.out.print( "Answer: ");
     }
 
     protected String manageJailerDP(int id, String department, DataBase dataBase)
