35569ec9c823b9db360a5c73db8d8b79007ad4da
Feyza Nur Akyol <fnuakyol@gmail.com>
2020-06-18 16:29:29 +0300

    Update ChiefJailer.java

--- Final Report/DataBase and ToDo/src/com/Group1/ChiefJailer.java
+++ Final Report/DataBase and ToDo/src/com/Group1/ChiefJailer.java
@@ -65,6 +65,64 @@ public class ChiefJailer extends Jailer {
         for ( k = 0; k < 45; k++) System.out.print("-");
         System.out.print("\n");
         System.out.print( "Answer: ");
+            
+        Scanner scan = new Scanner(System.in);
+        int answer = scan.nextInt();
+
+
+        switch (answer){
+            case 1:
+                for ( k = 0; k < 45; k++) System.out.print("-"); System.out.println();
+                System.out.println("Enter name: ");
+                String name = scan.nextLine();
+                for ( k = 0; k < 45; k++) System.out.print("-"); System.out.println();
+                System.out.println("Enter surname: ");
+                String surname = scan.nextLine();
+                for ( k = 0; k < 45; k++) System.out.print("-"); System.out.println();
+                System.out.println("Enter TC number: ");
+                String tc = scan.nextLine();
+                for ( k = 0; k < 45; k++) System.out.print("-"); System.out.println();
+                System.out.println("Enter inmate id: ");
+                String inmateNumber = scan.nextLine();
+                for ( k = 0; k < 45; k++) System.out.print("-"); System.out.println();
+                System.out.println("Enter telephone number of visitor: ");
+                String telephone = scan.nextLine();
+                for ( k = 0; k < 45; k++) System.out.print("-"); System.out.println();
+                System.out.println("Enter date: ");
+                String date = scan.nextLine();
+                for ( k = 0; k < 45; k++) System.out.print("-"); System.out.println();
+                System.out.println("Enter enterance time: ");
+                String time = scan.nextLine();
+
+                addVisitor(new Visitor(name,surname,tc,inmateNumber,telephone,date,time),dataBase);
+
+                break;
+            case 2:
+                removeVisitor(dataBase);
+                break;
+            case 3:
+                clearVisitors(dataBase);
+                break;
+            case 4:
+                for ( k = 0; k < 45; k++) System.out.print("-"); System.out.println();
+                System.out.println("Enter ID of the prison: ");
+                int id = scan.nextInt();
+                System.out.println(dataBase.getInmate(id).toString());
+                break;
+            case 5:
+                System.out.println("Shift hours:" );
+                for (int i = 0; i < getShiftHours().size() ; i++) {
+                    System.out.println(getShiftHours().get(i));
+                }
+                break;
+            case 6:
+                System.out.println("Department:" + getDP());
+                break;
+
+            default:
+                break;
+
+        }
     }
 
     protected String manageJailerDP(int id, String department, DataBase dataBase)
